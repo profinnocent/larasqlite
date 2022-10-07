5a8c35e29deeaeb63302cac10969719e5d4b733d
Innocent Unachukwu <innocent.unachukwu@gmail.com>
2022-10-07 12:28:54 +0100

    updated procfile

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/alert', function (Request $request) {
+    return Response("This the larasqlite alert");
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
